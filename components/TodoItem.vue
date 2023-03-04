cc7c6ca24b567a83b2151a1e76218a1eb4a63f3a
isakura313 <p.yakupov@walleti.com>
2023-03-04 20:59:04 +0200

    fix

--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -1,3 +1,14 @@
 <template>
-  Здесь список дел
+    <div class="todo_item">
+        <div class="todo_item__icon_wrap">
+            <IconCompleted/>
+        </div>
+
+    </div>
 </template>
+
+
+<script lang="ts" setup>
+import {ref} from 'vue';
+import IconCompleted from './icons/IconСompleted.vue'
+</script>
